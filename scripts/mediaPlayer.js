5ec10a8549b7c85ceb4028a0162707aacb91f4f5
Black <blackheart.frt@gmail.com>
2022-01-07 20:27:04 -0300

    em progresso: carregar nomes das faixas numa lista

--- scripts/mediaPlayer.js
+++ scripts/mediaPlayer.js
@@ -1,14 +1,82 @@
+/*======================================================
+    MEDIA PLAYER: ELEMENTOS DA UI
+======================================================*/
+const
+    playlistBanner = document.getElementById('playlist-banner'),
+    playlistName = document.getElementById('playlist-name'),
+    playlistDetails = document.getElementById('playlist-details'),
+    playlistContainer = document.getElementById('playlist-container'),
+    trackPlayingNow = document.getElementById('track-playingNow'),
+    trackArtist = document.getElementById('track-artist'),
+    trackCurrentTime = document.getElementById('track-currentTime'),
+    trackSeekerSlider = document.getElementById('track-seekerSlider'),
+    trackDuration = document.getElementById('track-duration'),
+    btnPrev = document.getElementById('btn-prev'),
+    btnPlay = document.getElementById('btn-play'),
+    btnNext = document.getElementById('btn-next');
+
+/*======================================================
+    MEDIA PLAYER: GLOBAIS
+======================================================*/
+let playlist = null,
+    actualTrack = 0,
+    isPlaying = false;
+
 //Elemento de áudio para o Player de música
 let audioElement = document.createElement('audio');
 
-//Faixa de áudio ou playlist à executar
-//let playlist = '/playlist/bensound-acousticbreeze.mp3';
-async function getPlaylist(url) {
+/*======================================================
+    MEDIA PLAYER: DATA
+    TODO: call in click event
+======================================================*/
+//DATA: Pega os dados da playlist à executar
+(async function getPlaylistData(url) {
     let response = await fetch(url);
-    return response;
+
+    if (response.status === 200) {
+        let json = await response.json();
+        playlist = json;
+        loadPlaylistData(json);
+    } else {
+        console.log(`HTTP-Error: ${response.status}`);
+    }
+})('http://localhost:5500/playlist/playlist.json');
+
+//DATA: Carregar dados da playlist no front
+function loadPlaylistData(data) {
+    let trackList = document.createElement('UL'),
+        trackListItem = document.createElement('LI');
+
+    trackList.setAttribute('id', 'trackList');
+
+    playlistContainer.appendChild(trackList);
+
+
+    let populateTracks = function(item) {
+        playlistName.innerText = item.name;
+        playlistBanner.style.backgroundImage = `url("/playlist/${item.name}/cover/${item.cover}")`;
+        // console.log(item);
+        // console.log(item.tracks);
+
+        for (let i = 0; i < item.tracks.length; i++) {
+            console.log(i + item.tracks[i].trackName)
+            if (trackList.childElementCount === 0) {
+                trackList.appendChild(trackListItem);
+            } else {
+                let trackListElement = document.getElementById('trackList');
+                console.log(trackListElement);
+                // TODO: adicionar li dentro da ul com nome das faixas
+            }
+        }
+
+    }
+
+    data.forEach(populateTracks);
 }
 
-
+/*======================================================
+    MEDIA PLAYER: AÇÕES
+======================================================*/
 //Ação: play()
 function play() {
     if (audioElement.src && audioElement.readyState === 4) {
